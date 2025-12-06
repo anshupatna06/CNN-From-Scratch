51373fed63ff9c8b82aa103277711975a1742b32
Decision Tree From scratch <anshupatna06@gmail.com>
2025-12-06 20:27:50 +0530

    Add filter and bias initialization functions

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,11 @@
+import numpy as np
+
+def initialize_filters(F, C, FH, FW):
+    return np.random.randn(F, C, FH, FW) * 0.01
+
+def initialize_bias(F):
+    return np.zeros(F)
+
+def print_shape(name, x):
+    print(f"{name} shape = {x.shape}")
 
